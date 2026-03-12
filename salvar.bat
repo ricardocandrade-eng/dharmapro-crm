736acc16db790168adbd5bb3927ebe6c8dc8f6d1
Ricardo Andrade <ricardocandrade-eng@gmail.com>
2026-03-12 00:31:00 -0300

    Sincronizando todos os arquivos do CRM

---
+++ salvar.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Atualizando arquivos do Google Apps Script...
+clasp pull
+echo.
+echo Preparando arquivos para o GitHub...
+git add .
+set /p msg="Digite o que voce mudou: "
+git commit -m "%msg%"
+echo.
+echo Enviando para o GitHub...
+git push origin main
+echo.
+echo Tudo pronto, Ricardo!
+pause
